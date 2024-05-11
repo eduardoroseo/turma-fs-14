046276cf1f54a108826981cfcc67ed57c0438372
Eduardo Roseo <eduardoroseo@gmail.com>
2024-05-11 13:24:19 -0300

    feat: resolucao javascript

--- 6-javascript/index.js
+++ 6-javascript/index.js
@@ -4,11 +4,30 @@ function soma(numA, numB) {
     return numA + numB
 }
 
-document.getElementById("btn-soma").addEventListener("click", function() {
+function multiplica(numA, numB) {
+    return numA * numB
+}
+
+function subtrai(numA, numB) {
+    return numA - numB
+}
+
+function divide(numA, numB) {
+    return numA - numB
+}
+
+document.getElementById("btn-calcular").addEventListener("click", function() {
     const inputNumA = document.getElementById("numA").value
     const inputNumB = document.getElementById("numB").value
 
-    const resultado = soma(Number(inputNumA), Number(inputNumB))
+    const resultadoSoma = soma(Number(inputNumA), Number(inputNumB))
+    const resultadoSub = subtrai(Number(inputNumA), Number(inputNumB))
+    const resultadoMulti = multiplica(Number(inputNumA), Number(inputNumB))
+    const resultadoDiv = divide(Number(inputNumA), Number(inputNumB))
 
-    document.getElementById("resultado").innerText = "O resultado da soma é: " + resultado
+    document.getElementById("resultado").innerHTML = 
+        "O resultado da soma é: " + resultadoSoma + "</br>" +
+        "O resultado da subtração é: " + resultadoSub + "</br>" +
+        "O resultado da multiplicação é: " + resultadoMulti + "</br>" +
+        "O resultado da divisão é: " + resultadoDiv
 })
