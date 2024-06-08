4c36a3ac0c320f84eda92c7424eb19ec803bbab4
Eduardo Roseo <eduardoroseo@gmail.com>
2024-06-08 11:37:52 -0300

    feat:  inserindo novo usuario

--- 6-javascript-consumindo-api/index.js
+++ 6-javascript-consumindo-api/index.js
@@ -5,7 +5,11 @@ async function carregarUsuarios() {
 
     document.getElementById('conteudo').innerHTML = `
         <tr class="text-center">
-            <td colspan="4">Carregando ....</td>
+            <td colspan="4">
+                <div class="spinner-border" role="status">
+                    <span class="visually-hidden"></span>
+                </div>
+            </td>
         </tr>
     `
       
@@ -16,8 +20,7 @@ async function carregarUsuarios() {
 }
 
 function alimentaTabela(usuarios) {
-    const htmlUsuarios = usuarios.map(usuario => {
-        return `
+    const htmlUsuarios = usuarios.map(usuario => `
             <tr>
                 <td>${usuario.id}</td>
                 <td>${usuario.nome}</td>
@@ -25,10 +28,57 @@ function alimentaTabela(usuarios) {
                 <td>${usuario.telefone}</td>
             </tr>
         `
-    })
+    )
 
     const htmlConteudo = htmlUsuarios.join("");
     document.getElementById('conteudo').innerHTML = htmlConteudo
 }
 
+async function salvarUsuario() {
+    const nome = document.getElementById("nome").value
+    const telefone = document.getElementById("telefone").value
+    const email = document.getElementById("email").value
+
+    if (!nome) {
+        Swal.fire({
+            title: `O campo nome esta vazio`,
+            icon: "error"
+        })
+        return;
+    }
+    if (!email) {
+        Swal.fire({
+            title: `O campo email esta inválido`,
+            icon: "error"
+        })
+        return;
+    }
+    if (!telefone) {
+        Swal.fire({
+            title: `O campo telefone esta inválido`,
+            icon: "error"
+        })
+        return;
+    }
+
+    const payload = {
+        nome: nome,
+        telefone: telefone,
+        email: email
+    }
+
+    const requestOptions = {
+        method: "POST",
+        body: JSON.stringify(payload)
+    };
+
+    await fetch("http://localhost:3000/usuarios", requestOptions)
+    
+    const modalElement = document.getElementById("modal-usuario");
+    bootstrap.Modal.getInstance(modalElement).hide()
+
+    carregarUsuarios()
+}
+
 document.addEventListener('DOMContentLoaded', carregarUsuarios)
+document.getElementById("btn-salvar-usuario").addEventListener('click', salvarUsuario)
